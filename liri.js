04a07ccc4a8061b03811bf3c38002bb41d744bb6
Harmin Jeong <harminjeong@Harmins-MacBook-Pro.local>
2018-01-08 01:52:08 -0500

    initial site files

---
+++ liri.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+var fs = require('fs');
+var keys = require('./keys');
+
+var twitter = require('twitter');
+var spotify = require('spotify');
+var request = require('request')
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+var arg = process.argv[2]
+var process = process.argv;
+
+
+var search="";
+
+for (var i = 3;i < process.length; i++) {
+    search += process[i] + "+";
+}
