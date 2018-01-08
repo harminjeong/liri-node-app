22c254395b10723a1407bb5b203dc4d7c0895095
Harmin Jeong <harminjeong@Harmins-MacBook-Pro.local>
2018-01-08 10:03:39 -0500

    add switch

--- liri.js
+++ liri.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 
 var fs = require('fs');
-var keys = require('./keys');
+var keys = require('./keys.js');
 
-var twitter = require('twitter');
-var spotify = require('spotify');
-var request = require('request')
+var spotify = require('node-spotify-api');
+var request = require('request');
 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
 
-var arg = process.argv[2]
+var arg = process.argv[2];
 var process = process.argv;
 
 
@@ -19,6 +16,38 @@ var search="";
 for (var i = 3;i < process.length; i++) {
     search += process[i] + "+";
 }
+
+switch (arg){
+    case "my-tweet":
+    tweet ();
+    break;
+    case "spotify-this-song":
+    spot ();
+    break;
+    case "movie-this":
+    movie ();
+    break;
+    case "do-what-it-says":
+    random ();
+    break;
+}
+
+function tweet () {
+    var keys = require('./keys.js');
+    var twitter = require('twitter');
+    var client = new Twitter(keys.twitter);
+    var parameter = {screen_name: hvrmin12, count: 20};
+    client.get('statuses/user_timeline', parameter, function(error,data){
+        if (error) {
+            console.log(error);
+        }
+        for (i=0; i < data.length;i++) {
+        console.log(data)
+        }
+    })
+}
+
+tweet();
 
 console.log(search);
 console.log(data);
